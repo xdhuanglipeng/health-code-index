8350484546b31fa1daa5a75d8685386b8d0c3baf
xdhuanglipeng <lipenghuang@yeah.net>
2022-11-12 12:05:34 +0800

    try fullscreen

--- common/base.js
+++ common/base.js
@@ -76,6 +76,30 @@ function navigateHome(){
     window.location.href="/";
 }
 
+/* 进入全屏 */
+function requestFullScreen(element = document.documentElement) {
+  if (element.requestFullscreen) {
+    element.requestFullscreen()
+  } else if (element.mozRequestFullScreen) {
+    element.mozRequestFullScreen()
+  } else if (element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen()
+  } else if (element.msRequestFullscreen) {
+    element.msRequestFullscreen()
+  }
+}
+
+/* 退出全屏 */
+function exitFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen()
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen()
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen()
+  }
+}
+
 function navigateToTripCard(){
     window.location.href="/health-code-index/trip-card";
 }
